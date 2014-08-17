70e6d06a3863dfc00d2c62acc75d3c964bbbddc3
monochromegane <dev.kuro.obi@gmail.com>
2014-08-17 10:59:54 +0900

    Added a Destination struct.

---
+++ destination.go
@@ -0,0 +1,21 @@
+package ship
+
+type Destination struct {
+	Name      string
+	Host      string
+	Variables Variables
+}
+
+type Variables map[string]string
+
+func (v Variables) Get(key string) string {
+	if value, ok := v[key]; ok {
+		return value
+	} else {
+		return ""
+	}
+}
+
+func (d Destination) Var(key string) string {
+	return d.Variables.Get(key)
+}
